a16b379f92aa78beaaab7a7434574e4255d90d5f
Daniil <d.istomin@we-are-talents.com>
2021-06-17 15:59:23 +0300

    Refactored unused files

--- main/views.py
+++ main/views.py
@@ -1,135 +1,48 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-import datetime as dt
-
 from . import models
+from .scripts import (
+    check_num,
+    to_arabic,
+    to_roman,
+    save_history,
+)
 
 
 class MainPage(TemplateView):
-	template_name = "main.html"
+    template_name = "main.html"
 
-	def get(self, request):
-		history = models.History.objects.all()
-		args = {"history":history}
-		
-		return render(request, self.template_name, context=args)
+    def get(self, request):
+        history = models.History.objects.all()
+        args = {"history": history}
 
-	def post(self, request):
+        return render(request, self.template_name, context=args)
 
-		def save_history(raw, num):
-			"""
-			save latest 6 conversion
-			revrite the oldest one
-			"""
-			if models.History.objects.all().count() > 6:
-				tmp = models.History.objects.latest()
-			else:
-				tmp = models.History()
+    def post(self, request):
+        # get raw string from html form and check it out
+        rawNumber = request.POST.get("numStr")
+        rawNumber = rawNumber.upper()
+        numType = check_num(rawNumber)
 
-			tmp.from_num = raw
-			tmp.to_num = num
-			tmp.time = dt.datetime.now()
-			tmp.save()
+        # use one of converter foo or return an error message
+        if numType == None or rawNumber == "":
+            res = "Incorrect input! Try again)"
+        elif rawNumber == "0":
+            res = 0
+        elif numType == "R":
+            res = to_arabic(rawNumber)
+            save_history(rawNumber, res)
+        elif numType == "A":
+            res = to_roman(rawNumber)
+            save_history(rawNumber, res)
+        else:
+            res = "Unexpected error! Try again)"
 
-		def check_num(num):
-			"""
-			check, is num an arabic or a roman 
-			return None, if no one
-			"""
-			all_models = models.Numbers.objects.all()
-			roman = [x.roman for x in all_models]
-			arabic = [str(x) for x in range(10)]
-			
-			num = list(num)
+        # get the history of conversions
+        history = models.History.objects.all()
 
-			if set(num).issubset(roman):
-				return "R"
+        # create diction of arguments - result of conv. and list of history
+        args = {"result": res, "history": history}
 
-			elif set(num).issubset(arabic):
-				return "A"
-
-			else:
-				return None
-
-		def to_roman(num):
-			"""
-			convert number from arabic to roman
-			return string of roman numbres
-			"""
-			num = int(num)
-
-			all_models = models.Numbers.objects.all()
-			nums = {x.arabic : x.roman for x in all_models}
-			keys = list(nums.keys())
-			keys.sort(reverse=True) #can be important!
-
-			numStr = ""
-
-			if num//1000 > 3:
-				numStr += "M*"+str(num//1000)+"+"
-				num -= (num//1000)*1000
-
-			for x in range(len(keys)):
-				amount = num // int(keys[x])
-
-				if amount > 3 and int(keys[x])!=1000:
-					numStr += nums[keys[x]] + nums[keys[x-1]]
-					num = num - (int(keys[x-1]) - int(keys[x]))
-
-				else:
-					for j in range(amount):
-						numStr += nums[keys[x]]
-					num = num - amount*int(keys[x])
-
-			return numStr
-
-		def to_arabic(num):
-			"""
-			convert number from roman to arabic
-			return integer 
-			"""
-			all_models = models.Numbers.objects.all()
-			nums = {str(x.roman) : x.arabic for x in all_models}
-			
-			num = list(num)
-			calc = []
-			res = 0
-
-			#convert roman to arabic
-			for x in num:
-				calc.append(int(nums[x]))
-
-			#calculate with the rules of roman number position
-			for x in range(len(calc)-1):
-				if calc[x] < calc[x+1]:
-					calc[x] = calc[x]*(-1)
-
-			return sum(calc)
-
-		#get raw string from html form and check it out
-		rawNumber = request.POST.get("numStr")
-		rawNumber = rawNumber.upper()
-		numType = check_num(rawNumber)
-
-		#use one of converter foo or return an error message
-		if numType == None or rawNumber=="":
-			res = "Incorrect input! Try again)"
-		elif rawNumber=="0":
-			res = 0
-		elif numType == "R":
-			res = to_arabic(rawNumber)
-			save_history(rawNumber, res)
-		elif numType == "A":
-			res = to_roman(rawNumber)
-			save_history(rawNumber, res)
-		else:
-			res = "Unexpected error! Try again)"
-
-		#get the history of conversions
-		history = models.History.objects.all()
-
-		#create diction of arguments - result of conv. and list of history
-		args = {"result":res, "history":history}
-
-		return render(request, self.template_name, context=args)
+        return render(request, self.template_name, context=args)
--- vserver/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rinejs~1\desktop\transl~1\vserver\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
