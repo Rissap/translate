8fe690bcc514e5daab9a20ad5a93ad6cb6801453
riisatoro <riisa.toro@gmail.com>
2022-03-08 10:53:14 +0200

    Fix history saving

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from . import models
+from main.models import History
 from main.convertors import (
     get_number_type,
     convert_to_arabic,
@@ -15,25 +15,31 @@ class MainPage(TemplateView):
     template_name = "main.html"
 
     def get(self, request):
-        history = models.History.objects.all()
+        history = History.objects.all()
         return render(request, self.template_name, {"history": history})
 
     def post(self, request):
         raw_number = request.POST.get("number")
         raw_number = raw_number.upper()
-        num_type = get_number_type(raw_number)
+        number_type = get_number_type(raw_number)
 
-        if not num_type:
+        if not number_type or not raw_number:
             return render(
-                request, self.template_name, {'result': 'Unexpected input. Try again'}
+                request, 
+                self.template_name, 
+                {
+                    'result': 'Unexpected input. Try again',
+                    'history': History.objects.all()
+                }, 
             )
 
-        if num_type == NumberType.ROMAN:
+        if number_type == NumberType.ROMAN:
             result = convert_to_arabic(raw_number)
         else:
             result = convert_to_roman(int(raw_number))
 
-        history = models.History.objects.all()
+        save_history(raw_number, result)
+        history = History.objects.all()
         args = {"result": result, "history": history}
 
         return render(request, self.template_name, args)
