2fb2de6e82f628f457451b553fbfdc0217ddb54b
riisatoro <riisa.toro@gmail.com>
2022-03-08 18:30:14 +0200

    Update git actions

--- server/settings.py
+++ server/settings.py
@@ -59,8 +59,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_NAME'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
