7050eafa809c32126cccaf26440596d6c6b470bb
riisatoro <riisa.toro@gmail.com>
2022-03-18 10:28:49 +0200

    Add global env installation to heroku

--- heroku-entrypoint.sh
+++ heroku-entrypoint.sh
@@ -2,8 +2,8 @@
 
 # Apply database migrations
 echo "Apply database migrations"
-poetry run python manage.py migrate
+python manage.py migrate
 
 # Start server
 echo "Starting server"
-poetry run python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
